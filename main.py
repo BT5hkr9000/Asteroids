87f9cb22cc8b8db5ba5b217741591c214fa03f0c
BT5hkr9000 <j-boldwin@live.co.uk>
2025-01-10 17:05:19 +0000

    Limited to 60fps

--- main.py
+++ main.py
@@ -12,12 +12,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}") # type: ignore
     print(f"Screen height: {SCREEN_HEIGHT}") # type: ignore
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
