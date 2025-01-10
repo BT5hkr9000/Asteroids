5389a9c037975d43b74e2c8c515746bc249dfb13
BT5hkr9000 <j-boldwin@live.co.uk>
2025-01-10 16:55:10 +0000

    Launchable and quittable application

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame # type: ignore
+from constants import * # type: ignore
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+def main():
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}") # type: ignore
+    print(f"Screen height: {SCREEN_HEIGHT}") # type: ignore
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
